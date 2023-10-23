0491496b4eeb0f7b8f3e91352d70f131826bf6ad
diomasaki <112537576+diomasaki@users.noreply.github.com>
2023-10-24 02:46:24 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "images.pexels.com", "www.gannett-cdn.com", "images.summitmedia-digital.com", "majalah.ottenstatic.com", "qph.cf2.quoracdn.net", "scontent.fcgk18-1.fna.fbcdn.net", "10619-2.s.cdn12.com", "i0.wp.com", "i.gojekapi.com"]
+    domains: ["diomasaki.github.io", "images.pexels.com", "www.gannett-cdn.com", "images.summitmedia-digital.com", "majalah.ottenstatic.com", "qph.cf2.quoracdn.net", "scontent.fcgk18-1.fna.fbcdn.net", "10619-2.s.cdn12.com", "i0.wp.com", "i.gojekapi.com"]
   },
 }
 
